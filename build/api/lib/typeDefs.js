8bffc23fec2671295e7c24343d8be140e7c42ad8
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-02-21 20:45:49 -0500

    feat: update all

--- build/api/lib/typeDefs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _merge = require("@graphql-tools/merge");
-
-var _loadFiles = require("@graphql-tools/load-files");
-
-const typesArray = (0, _loadFiles.loadFilesSync)('**/*.gql');
-module.exports = (0, _merge.mergeTypeDefs)(typesArray);
