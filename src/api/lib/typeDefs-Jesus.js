dd71d44d0bdfc80c37541c0ed281299a5a519222
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-09-12 14:54:09 -0500

    feat: update cors

--- src/api/lib/typeDefs-Jesus.js
+++
@@ -1,5 +0,0 @@
-import { mergeTypeDefs } from '@graphql-tools/merge'
-import { loadFilesSync } from '@graphql-tools/load-files'
-
-const typesArray = loadFilesSync('**/*.gql')
-module.exports = mergeTypeDefs(typesArray)
