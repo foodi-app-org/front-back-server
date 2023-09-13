125af0db3ef09ff796010dcb3206e020f2708542
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-09-13 12:01:02 -0500

    feat: update create banner

---
+++ src/api/lib/resolvers/admin/Banner/index.js
@@ -0,0 +1,89 @@
+import banners from '../../../models/banners'
+import { Op } from 'sequelize'
+
+const setPromoBanners = async (parent, args, context, info) => {
+    try {
+      // Extrae los datos del argumento de entrada
+      const { input } = args;
+
+      // Crea un nuevo banner en la base de datos utilizando el modelo 'Banner'
+      const newBanner = await banners.create({
+        path: input.path,
+        description: input.description,
+        bpState: input.bpState,
+        name: input.name,
+      });
+
+      // Devuelve el banner recién creado como resultado
+      return newBanner;
+    } catch (error) {
+      // Manejo de errores: puedes personalizar el manejo de errores según tus necesidades
+      console.error('Error al crear un nuevo banner:', error);
+      throw new Error('No se pudo crear el banner. Por favor, inténtalo de nuevo más tarde.');
+    }
+  };
+  // Define el resolver para la mutación setBanners
+const setBanners = async (parent, args, context, info) => {
+    try {
+      // Extrae los datos del argumento de entrada
+      const { input } = args;
+
+      // Crea un nuevo banner en la base de datos utilizando el modelo "Banner"
+      const newBanner = await banners.create({
+        path: input.path,
+        description: input.description,
+        BannersState: input.BannersState,
+        name: input.name,
+      });
+      // Devuelve el banner recién creado como resultado
+      return newBanner;
+    } catch (error) {
+      // Manejo de errores: puedes personalizar el manejo de errores según tus necesidades
+      console.error('Error al crear un nuevo banner:', error);
+      throw new Error('No se pudo crear el banner. Por favor, inténtalo de nuevo más tarde.');
+    }
+  };
+  
+  const getAllPromoBanners = async (parent, args, context, info) => {
+    try {
+      // Extrae los datos de los argumentos de entrada
+      const { input } = args;
+
+      // Construye una consulta de búsqueda basada en los parámetros opcionales
+      const whereClause = {};
+      if (input.search) {
+        whereClause.name = { [Op.like]: `%${input.search}%` };
+      }
+      if (input.min) {
+        whereClause.bpId = { [Op.gte]: input.min };
+      }
+      if (input.max) {
+        whereClause.bpId = { [Op.lte]: input.max };
+      }
+
+      // Realiza la consulta a la base de datos utilizando el modelo "Banner"
+      const dataBanners = await banners.findAll({
+        where: whereClause,
+        attributes: ['path', 'description', 'createAt', 'name', 'updateAt'],
+      });
+
+      // Devuelve los banners encontrados como resultado
+      return dataBanners;
+    } catch (error) {
+      // Manejo de errores: puedes personalizar el manejo de errores según tus necesidades
+      console.error('Error al obtener los banners promocionales:', error);
+      throw new Error('No se pudieron obtener los banners promocionales. Por favor, inténtalo de nuevo más tarde.');
+    }
+  };
+
+export default {
+    TYPES: {
+    },
+    QUERIES: {
+      getAllPromoBanners
+    },
+    MUTATIONS: {
+        setBanners,
+        setPromoBanners
+    }
+  }
--- src/api/lib/resolvers/admin/index.js
+++ src/api/lib/resolvers/admin/index.js
@@ -1,14 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
 import adminResolver from './admin'
+import bannerResolver from './Banner'
 
 export default {
   TYPES: {
-    ...adminResolver.TYPES
+    ...adminResolver.TYPES,
+    ...bannerResolver.TYPES
   },
   QUERIES: {
-    ...adminResolver.QUERIES
+    ...adminResolver.QUERIES,
+    ...bannerResolver.QUERIES
   },
   MUTATIONS: {
-    ...adminResolver.MUTATIONS
+    ...adminResolver.MUTATIONS,
+    ...bannerResolver.MUTATIONS
   }
 }
