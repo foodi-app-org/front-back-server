a0e901ae189fc0c8584821d1ed3d5744ce080c37
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-10-02 19:34:58 -0500

    feat: change domain

--- src/api/lib/router/index.js
+++ src/api/lib/router/index.js
@@ -16,6 +16,7 @@ export const cookie = {
     maxAge: 60 * 60 * 8, // 8 horas
     httpOnly: true,
     path: "/",
+    domain: 'eatsy-admin-store.vercel.app',
     secure: process.env.NODE_ENV === 'production', // Ajusta a true en producción
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' // Configura 'none' en producción
   }
